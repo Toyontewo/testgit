17804000eb752c740b8c78cb6e9f7e049f04cdbb
toyontewo <samuelntw4lyf@gmail.com>
2025-06-17 13:08:35 +0100

    Snakke game 2

---
+++ main.py
@@ -0,0 +1,31 @@
+import time
+from snake import Snake
+from turtle import Screen, Turtle
+
+screen = Screen()
+screen.setup(600, 600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+## screen.update()
+
+
+game_on = True
+while game_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
+
+
+
+screen.exitonclick()
